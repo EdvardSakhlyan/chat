8b91f804c6668f26d0712391e7404b61eef49098
Edvard Sakhlyan <edvard138@mail.ru>
2023-07-22 22:22:44 +0400

    first commit

---
+++ front/src/components/SideBar/SideBar.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react"
+
+interface ISideBarProps {
+    routs: []
+}
+
+const SideBar: FC<ISideBarProps> = () => {
+    return (
+        <nav>
+            <ul>
+                {
+
+                }
+            </ul>
+        </nav>
+    )
+}
+
+export default SideBar
